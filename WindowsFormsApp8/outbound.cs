5bd5701077386e26cd12a9517b05d0957fb4eda5
LIGHT <2277978888@qq.com>
2020-11-01 23:53:10 +0800

    22

--- WindowsFormsApp8/outbound.cs
+++ WindowsFormsApp8/outbound.cs
@@ -2,6 +2,7 @@
 using System.Data;
 using System.Windows.Forms;
 using WindowsFormsApp7;
+using Microsoft.VisualBasic;
 using MySql.Data.MySqlClient;
 
 namespace WindowsFormsApp8
@@ -82,7 +83,7 @@ namespace WindowsFormsApp8
                 String value1 = dataGridView1.Rows[index].Cells[1].Value.ToString();
                 String value2 = dataGridView1.Rows[index].Cells[2].Value.ToString();
                 String value3 = dataGridView1.Rows[index].Cells[3].Value.ToString();
-                String value4= dataGridView1.Rows[index].Cells[4].Value.ToString();
+                String value4 = dataGridView1.Rows[index].Cells[4].Value.ToString();
                 String value5 = dataGridView1.Rows[index].Cells[5].Value.ToString();
                 String value6 = dataGridView1.Rows[index].Cells[6].Value.ToString();
                 String value7 = dataGridView1.Rows[index].Cells[7].Value.ToString();
@@ -90,7 +91,7 @@ namespace WindowsFormsApp8
 
                 String sql = String.Format(
                     "update outbound set outbound_shop_name = '{0}',outbound_shop_price='{1}',outbound_shop_number='{2}',outbound_to_person='{3}',outbound_to_phone = '{4}',outbound_to_address = '{5}',outbound_data='{6}' where outbound_id='{7}'",
-                    value1, value2, value3, value4, value5, value6,value7, value0);
+                    value1, value2, value3, value4, value5, value6, value7, value0);
                 MySqlCommand comm = new MySqlCommand(sql, conn);
                 comm.ExecuteNonQuery();
                 MessageBox.Show("修改成功");
@@ -129,14 +130,115 @@ namespace WindowsFormsApp8
             {
                 conn.Close();
             }
-            
-            
-            
         }
 
         private void submit1_Click(object sender, EventArgs e)
         {
-            
+            Database2 db = new Database2();
+            MySqlConnection conn = db.getConn();
+            try
+            {
+                conn.Open();
+                String sql = String.Format("select count(*) from outbound ");
+                MySqlCommand comm = new MySqlCommand(sql, conn);
+                int out_bound_num = Convert.ToInt32(comm.ExecuteScalar());
+                if (out_bound_num > 0)
+                {
+                    for (int i = 1; i <= out_bound_num; i++)
+                    {
+                        String sql1 = String.Format("select * from outbound limit {0}", i.ToString());
+                        MySqlCommand comm1 = new MySqlCommand(sql1, conn);
+                        MySqlDataAdapter sda = new MySqlDataAdapter();
+                        sda.SelectCommand = comm1;
+                        //数据集
+                        DataSet ds = new DataSet();
+                        sda.Fill(ds, "outbound");
+                        String shop_name = ds.Tables[0].Rows[0]["OUTBOUND_SHOP_NAME"].ToString().Trim();
+                        String shop_number = ds.Tables[0].Rows[0]["outbound_shop_number"].ToString().Trim();
+
+                        MessageBox.Show(shop_name);
+                        MessageBox.Show(shop_number);
+                        String sql2 = String.Format("select * from stock_store where stock_shop_name = '{0}'",
+                            shop_name);
+                        MySqlCommand comm2 = new MySqlCommand(sql2, conn);
+                        MySqlDataAdapter sda1 = new MySqlDataAdapter();
+                        sda1.SelectCommand = comm2;
+                        DataSet ds1 = new DataSet();
+                        sda1.Fill(ds1, "stock_store");
+                        if (Convert.ToInt32(comm2.ExecuteScalar()) > 0)
+                        {
+                            String stock_number = ds1.Tables[0].Rows[0]["stock_shop_number"].ToString().Trim();
+                            if (Convert.ToInt32(stock_number) > Convert.ToInt32(shop_number))
+                            {
+                            }
+                            else
+                            {
+                                MessageBox.Show("库存不足");
+                                String num2 = Interaction.InputBox("库存剩余量" + stock_number + ",是否选择选择剩余库存量进行提交", "订单处理",
+                                    "", 3, 3);
+                                if (num2 == "")
+                                {
+                                    MessageBox.Show("该订单请求已经取消");
+                                }
+                                else
+                                {
+                                   String sql3 = String.Format("update stock_store set stock_shop_number = {0}-{1} where stock_shop_name = '{2}' ",stock_number,num2,shop_name);    
+                                   MySqlCommand comm3 = new MySqlCommand(sql3,conn);
+                                   int num =   comm3.ExecuteNonQuery();
+                                   if (num>0)
+                                   {
+                                      String sql5 = String.Format("update outbound set outbound_shop_number = {0}-{1} where outbound_shop_name = {2}",shop_number,num2,shop_name);
+                                      MySqlCommand comm5= new MySqlCommand(sql5,conn);
+                                      int num5 =     comm5.ExecuteNonQuery();
+                                      if (num5>0)
+                                      {
+                                          MessageBox.Show("该订单提交成功");
+                                      }
+                                      else
+                                      {
+                                          MessageBox.Show("该订单提交失败");
+                                      }
+                                   }
+                                   else
+                                   {
+                                       MessageBox.Show("订单处理失败");
+                                       // String sql4 = String.Format("update stock_store set stock_number = {0}+{1} where stock_shop_name = '{2}' ",stock_number,shop_number,shop_name);    
+                                       // MySqlCommand comm4 = new MySqlCommand(sql4,conn);
+                                       //  int num4 =   comm4.ExecuteNonQuery();
+                                       //  if (num4>0)
+                                       //  {
+                                       //      
+                                       //  }
+                                       //  else
+                                       //  {
+                                       //      MessageBox.Show("");
+                                       //  }
+                                   }
+                                   
+
+                                }
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("无该订单库存");
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("处理订单为空");
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+                MessageBox.Show(exception.Message.ToString());
+            }
+            finally
+            {
+                conn.Close();
+            }
         }
     }
 }
